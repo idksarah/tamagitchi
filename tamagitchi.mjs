5b35b1751875ffd58972753462d78ba16fb884fe
idksarah <sarah.p.ngai@gmail.com>
2025-06-09 23:39:08 -0700

    local files -> cdn links

--- tamagitchi.mjs
+++ tamagitchi.mjs
@@ -1,8 +1,8 @@
 const emotions = new Map();
-emotions.set("excited", "./emotions/excited.gif");
-emotions.set("happy", "./emotions/happy.gif");
-emotions.set("neutral", "./emotions/neutral.gif");
-emotions.set("sad", "./emotions/sad.gif");
+emotions.set("excited", "https://hc-cdn.hel1.your-objectstorage.com/s/v3/7d352400490c50770430f069bf846b1b0d4abbce_excited.gif");
+emotions.set("happy", "https://hc-cdn.hel1.your-objectstorage.com/s/v3/52a51244037834d41778e57a5599cb98420597ad_happy.gif");
+emotions.set("neutral", "https://hc-cdn.hel1.your-objectstorage.com/s/v3/84c5779df6447a2f74ba16641f7bffe63616ef6d_neutral.gif");
+emotions.set("sad", "https://hc-cdn.hel1.your-objectstorage.com/s/v3/0205ed49bbf9d97b25b0e1133752f37100932990_sad.gif");
 export class tamagitchi {
     constructor(emotion, url){
         this.emotion = emotion;
