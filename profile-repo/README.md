cf5f5be9153b19f64a9584592c970944320232be
idksarah <sarah.p.ngai@gmail.com>
2025-06-11 19:48:42 -0700

    hopefully update whenver someone stars

--- profile-repo/README.md
+++ profile-repo/README.md
@@ -3,10 +3,10 @@
 <p> find me on the hackclub slack @idksarah!</p>
 <hr class="solid">
 <div align="center">
-<img style="width: 50em;" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/84c5779df6447a2f74ba16641f7bffe63616ef6d_neutral.gif" alt="tamagitchi" /><br>
+<img style="width: 50em;" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/7d352400490c50770430f069bf846b1b0d4abbce_excited.gif" alt="tamagitchi" /><br>
 
-<p>octocat is feeling neutral because idksarah hasn't made a commit *today* but has made >= one commit in the <i>past three days</i>.</p>
-<p>pet them to make them excited! (<a href="https://github.com/idksarah/tamagitchi">star idksarah's tamagitchi!! ⭐</a>)</p>
+<p>octocat is feeling excited because idksarah received a star on tamagitchi!</p>
+<p>petting them can't make them any happier, but it sure will make idksarah happy! (<a href="https://github.com/idksarah/tamagitchi">star idksarah's tamagitchi!! ⭐</a>)</p>
 
-<p>last updated: 00:53 pst</p>
+<p>last updated: 19:34 utc | tamagitchi takes 1-2 minutes to update, please be patient <3 </p>
 </div>
--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import { Octokit } from "octokit";
 import dotenv from "dotenv";
-import fs from "fs";
+import fs, { stat } from "fs";
 import { getEmotionUrl, tamagitchi } from "./tamagitchi.mjs";
 import stats from "./stats.json" with {type: "json"};
 
@@ -12,18 +12,18 @@ const octokit = new Octokit({
 
 const DAY = 24 * 60 * 60 * 1000; 
 
-// !! edit this to your username and the name of the repo you'd like to highlight
+// !! add your username and the name of the repo you'd like to highlight
 const username = "idksarah";
 const highlightedRepo = "tamagitchi";
 
-// !! edit this to add your own README content!
+// !! add your own README content here!
 const otherContent= `<h1> hi!! i'm sarah</h1>
 <p> i like coding. sometimes :3 </p>
 <p> find me on the hackclub slack @idksarah!</p>
 <hr class="solid">`;
 
 const excitedContent=`
-<p>octocat is feeling excited because ${username} received a follow or star!</p>
+<p>octocat is feeling excited because ${username} received a star on <i>${highlightedRepo}</i>!</p>
 <p>petting them can't make them any happier, but it sure will make ${username} happy! (<a href="https://github.com/${username}/${highlightedRepo}">star ${username}'s ${highlightedRepo}!! ⭐</a>)</p>`;
 const happyContent=`
 <p>octocat is feeling happy because ${username} has made >= one commit today!</p>
@@ -35,69 +35,83 @@ const sadContent=`
 <p>octocat is feeling excited because ${username} hasn't had any activity in the <i>past three days</i>.</p>
 <p>pet them to make them excited! (<a href="https://github.com/${username}/${highlightedRepo}">star ${username}'s ${highlightedRepo}!! ⭐</a>)</p>`;
 
+const updateFrequency = 24;
+
+let userRes, repoRes;
+
 const main = async () => {
-    const userRes = await octokit.rest.users.getByUsername({username});
+    userRes = await octokit.rest.users.getByUsername({username});
 
-    const publicActivity = await octokit.rest.activity.listPublicEventsForUser({
-        username: `${userRes.data.login}`,
-        per_page: 10
-    });
-
-    const repoRes = await octokit.rest.repos.get({
+    repoRes = await octokit.rest.repos.get({
          owner: `${userRes.data.login}`,
          repo: highlightedRepo
-     });
+    });
 
-    let recentAct = [], olderAct = [];
+    console.log(repoRes.data.stargazers_count);
+    console.log(stats.stargazers_count);
 
-    publicActivity.data.forEach(event => {
-        let date = new Date(event.created_at);
-        if(date.getTime() >= (Date.now() - (DAY))){
-            recentAct.push(event);
-        } else if (date.getTime() >= (Date.now() - 3 * DAY)){
-            olderAct.push(event);
-        }
-    })
+     if (repoRes.data.stargazers_count > stats.stargazers_count){
+        console.log("?");
+        tamagitchi.pet.emotion = "excited";
 
-    // sort activity into emotions
-    if (recentAct.length != 0){
-        if (userRes.data.followers > stats.followers || repoRes.data.stargazers_count > stats.stargazers_count){
-            tamagitchi.pet.emotion = "excited";
-        } else if(recentAct.length >= 1){
-            if(recentAct.length >= 10){
+        // repeating these 2 bc tamagitchi shouldn't update if the star count hasn't changed or correct time has passed
+        updateStats();
+        updateReadme(tamagitchi.pet.emotion, getEmotionUrl(tamagitchi.pet.emotion));
+    } else if (Date.now() > (stats.lastUpdated + (DAY / updateFrequency))){
+        console.log("huh");
+        const publicActivity = await octokit.rest.activity.listPublicEventsForUser({
+            username: `${userRes.data.login}`,
+            per_page: 5 // adjust later
+        });
+
+        let recentAct = [], olderAct = [];
+
+        publicActivity.data.forEach(event => {
+            let date = new Date(event.created_at);
+            if(date.getTime() >= (Date.now() - (DAY))){
+                recentAct.push(event);
+            } else if (date.getTime() >= (Date.now() - 3 * DAY)){
+                olderAct.push(event);
+            }
+        })
+
+        // sort activity into emotions
+        if (recentAct.length != 0){if(recentAct.length >= 1){
+            if(recentAct.length >= 15){
                 tamagitchi.pet.emotion = "excited";
             } else {
                 tamagitchi.pet.emotion = "happy";
             }
-        } 
-    } else {
-        if (olderAct.length == 0 ){ 
-            tamagitchi.pet.emotion = "sad";
+            } 
         } else {
-            tamagitchi.pet.emotion = "neutral";
+            if (olderAct.length == 0 ){ 
+                tamagitchi.pet.emotion = "sad";
+            } else {
+                tamagitchi.pet.emotion = "neutral";
+            }
         }
-    }
+        
+        console.log(recentAct.length);
+        console.log(olderAct.length);
 
-    // update stats.json
-    if (Date.now() > (stats.lastUpdated + DAY)){
-        stats.lastUpdated = Date.now();
-        stats.followers = userRes.data.followers;
-        stats.stars = repoRes.data.stargazers_count;
-        fs.writeFileSync("./stats.json", JSON.stringify(stats, null, 2));
+        updateStats();
+        updateReadme(tamagitchi.pet.emotion, getEmotionUrl(tamagitchi.pet.emotion));
     }
-
-    // update README.md
-    const time = Date().toString().toLowerCase().substring(16,21);
-    const readMeContent = generateReadme(tamagitchi.pet.emotion, getEmotionUrl(tamagitchi.pet.emotion), time);
-    fs.writeFileSync("./profile-repo/README.md", readMeContent);
 
     console.log(tamagitchi.pet.emotion);
-    console.log(recentAct.length);
-    console.log(olderAct.length);
+
 };
 
+function updateStats(){
+    stats.lastUpdated = Date.now();
+    stats.followers = userRes.data.followers;
+    stats.stargazers_count = repoRes.data.stargazers_count;
+    console.log("file syncin json");
+    fs.writeFileSync("./src/stats.json", JSON.stringify(stats, null, 2));
+}
 
-function generateReadme(emotion, url, time){
+
+function updateReadme(emotion, url){
     let emotionContent;
     switch (emotion){
         case "excited":
@@ -112,13 +126,14 @@ function generateReadme(emotion, url, time){
         case "sad":
             emotionContent = sadContent;
     }
-
-    return `${otherContent}
+    const time = Date().toString().toLowerCase().substring(16,21);
+    const readMeContent = `${otherContent}
 <div align="center">
 <img style="width: 50em;" src="${url}" alt="tamagitchi" /><br>
 ${emotionContent}
 
-<p>last updated: ${time} pst</p>
+<p>last updated: ${time} utc | tamagitchi takes 1-2 minutes to update, please be patient <3 </p>
 </div>`;
+    fs.writeFileSync("./profile-repo/README.md", readMeContent);
 }
 main();
